685a56e2e9a5f0d93cdbbeb8ebb0b6ce6b31ef53
magican09 <maga26497@mail.ru>
2021-04-11 14:28:58 +0300

    Добавлен SQL запрос по второму заданию

---
+++ SqlQuery_1.sql
@@ -0,0 +1,5 @@
+﻿SELECT products.*, GROUP_CONCAT(categories.title) AS categ
+    FROM products
+    INNER JOIN categories_to_products ON products.id =  categories_to_products.product_id
+    LEFT JOIN categories ON categories_to_products.category_id = categories.id                     
+    GROUP BY products.title 
